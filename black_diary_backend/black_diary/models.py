1ca1a4cc40591aada3b22cf975ff990102fa263e
Josher89 <j.hernandezruiz89@gmail.com>
2024-12-31 16:34:58 -0800

    Forgot to make migrations. Changes have been migrated.

--- black_diary_backend/black_diary/models.py
+++ black_diary_backend/black_diary/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class NewPage(models.Model):
     title = models.CharField(max_length=250, blank=False)
     text = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
